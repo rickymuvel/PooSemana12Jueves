fc677fa5e04dc2f51d2b8ca3009d26f08375d5d4
yo <yo@localhost>
2015-06-04 22:33:15 -0500

    Segundo envío

--- APP_Clientes/Form1.vb
+++ APP_Clientes/Form1.vb
@@ -1,3 +1,12 @@
-﻿Public Class Form1
+﻿Imports APP_Clientes.ServiceClientes
 
+Public Class Form1
+    Dim servicio As New ServicioClienteClient
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cboClientes.DataSource = servicio.ClienteListar.Tables(0)
+        cboClientes.DisplayMember = "nomCliente"
+        cboClientes.ValueMember = "idCliente"
+    End Sub
 End Class
